855a48a7031477653f690ff28c6884c838ebcc65
Web developer Nasim <61669109+Developer-Nasim@users.noreply.github.com>
2020-12-19 03:54:27 +0600

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="utf-8">
     <meta http-equiv="x-ua-compatible" content="ie=edge">
-    <title>Esther - Car Accessories Shop HTML Template </title>
+    <title>Esther </title>
     <meta name="description" content="">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <!-- Favicon -->
